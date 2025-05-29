fcc0ee204d8f87fa63c7d0b40e7cc2d3ae1044f3
Max Große <max12.gro@gmail.com>
2025-05-29 21:55:15 +0200

    we sopping hard

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-
+  inputs.sops-nix.url = "github:Mic92/sops-nix";
+  inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   outputs =
     {
       nixpkgs,
       disko,
+      sops-nix,
       ...
     }:
     {
@@ -14,6 +16,7 @@
         system = "aarch64-linux";
         modules = [
           disko.nixosModules.disko
+          sops-nix.nixosModules.sops
           ./hosts/windrunner
         ];
       };
--- hosts/windrunner/default.nix
+++ hosts/windrunner/default.nix
@@ -2,8 +2,12 @@
   modulesPath,
   lib,
   pkgs,
+  config,
   ...
 }:
+let
+  smb_secrets = "/etc/nixos/smb-secrets";
+in
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -52,7 +56,7 @@
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+      [ "${automount_opts},credentials=${smb_secrets}" ];
   };
 
   users.users.root.openssh.authorizedKeys.keys = [
@@ -65,4 +69,23 @@
     "nix-command"
     "flakes"
   ];
+
+  # This will add secrets.yml to the nix store
+  # You can avoid this by adding a string to the full path instead, i.e.
+  # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
+  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  # This is using an age key that is expected to already be in the filesystem
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  # This will generate a new key if the key specified above does not exist
+  sops.age.generateKey = true;
+  # This is the actual specification of the secrets.
+  sops.secrets.storage = { };
+
+  systemd.services."storage-secret" = {
+    script = ''
+      echo username=u462951 > ${smb_secrets}
+      echo password=$(cat ${config.sops.secrets.storage.path}) > ${smb_secrets}
+    '';
+  };
 }
