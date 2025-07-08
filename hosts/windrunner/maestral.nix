5ba1b8971a04260ac9d8cc3f1bcafa7d001ce78b
Max Große <max12.gro@gmail.com>
2025-07-08 16:04:25 +0200

    just write it yourself

--- hosts/windrunner/maestral.nix
+++ hosts/windrunner/maestral.nix
@@ -1,36 +1,44 @@
 { pkgs, config, ... }:
 let
   dropbox_auth_key = config.users.users.root.home + "/.local/share/python_keyring/keyring_pass.cfg";
-  maestral_config_service = "maestral_config";
+  # maestral_config_service = "maestral_config";
 in
+
 {
+  # see https://github.com/samschott/maestral/issues/992
   systemd.user.services.maestral = {
     description = "Maestral";
     wantedBy = [ "default.target" ];
-    after = [ (maestral_config_service + ".service") ];
+    # after = [ (maestral_config_service + ".service") ];
     serviceConfig = {
       ExecStart = "${pkgs.maestral}/bin/maestral start";
       ExecReload = "${pkgs.util-linux}/bin/kill -HUP $MAINPID";
       KillMode = "process";
       Restart = "on-failure";
+      ExecPreStart = [
+        "mkdir -p /root/.config/maestral"
+        "echo 'executing pre start'"
+        "echo '${builtins.readFile ./maestral.ini}'"
+        "echo '${builtins.readFile ./maestral.ini}' > /root/.config/maestral/maestral.ini"
+      ];
     };
   };
 
-  systemd.user.services."${maestral_config_service}" = {
-    description = "Maestral config file";
-    after = [ "multi-user.target" ];
-    wantedBy = [ "multi-user.target" ];
-    script = ''
-      echo "running maestral config service!"
-      echo "testing" > /root/test.file
-      echo "${builtins.readFile ./maestral.ini}" > /root/.config/maestral/maestral.ini
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      User = "root";
-    };
-  };
+  # systemd.user.services."${maestral_config_service}" = {
+  #   description = "Maestral config file";
+  #   after = [ "multi-user.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   script = ''
+  #     echo "running maestral config service!"
+  #     echo "testing" > /root/test.file
+  #     echo "${builtins.readFile ./maestral.ini}" > /root/.config/maestral/maestral.ini
+  #   '';
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     RemainAfterExit = true;
+  #     User = "root";
+  #   };
+  # };
 
   sops.secrets.maestral_key_file = {
     path = dropbox_auth_key;
