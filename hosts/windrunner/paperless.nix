621c28f457799f34b994abc1e6596adb8e1dc4e7
Max Große <max12.gro@gmail.com>
2025-07-09 23:02:39 +0200

    now we make it secure

--- hosts/windrunner/paperless.nix
+++ hosts/windrunner/paperless.nix
@@ -1,21 +1,31 @@
 let
   port = 28981;
+  config_file = "/var/lib/config-paperless/config";
+  password_file = "/var/lib/config-paperless/passwd";
 in
 {
   services.paperless = {
     enable = true;
     consumptionDir = "/var/lib/maestral/paperless";
+    environmentFile = config_file;
+    passwordFile = password_file;
     settings = {
       PAPERLESS_URL = "https://paperless.maxrn.dev";
       PAPERLESS_OCR_LANGUAGE = "deu+eng";
     };
-  };
 
-  services.caddy = {
-    enable = true;
-    virtualHosts."paperless.maxrn.dev".extraConfig = ''
-      reverse_proxy http://127.0.0.1:${toString port}
-    '';
-  };
+    services.caddy = {
+      enable = true;
+      virtualHosts."paperless.maxrn.dev".extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString port}
+      '';
+    };
 
+    sops.secrets.paperless_config = {
+      path = config_file;
+    };
+    sops.secrets.paperless_password = {
+      path = password_file;
+    };
+  };
 }
