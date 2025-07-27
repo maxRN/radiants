a5d1d6a71343b014b533935999184b1ae36bc0f3
Max Große <max12.gro@gmail.com>
2025-07-27 14:10:40 +0200

    disable paperless

--- hosts/windrunner/paperless.nix
+++ hosts/windrunner/paperless.nix
@@ -6,7 +6,7 @@ let
 in
 {
   services.paperless = {
-    enable = true;
+    enable = false;
     package = paperless-pkgs.paperless-ngx;
     consumptionDir = "/var/lib/maestral/paperless";
     environmentFile = config_file;
