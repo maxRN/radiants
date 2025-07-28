05fb5878f40261b3affb808c6d5f97435cd21b48
Max Große <max12.gro@gmail.com>
2025-07-28 10:07:36 +0200

    actually include it...

--- hosts/windrunner/default.nix
+++ hosts/windrunner/default.nix
@@ -19,6 +19,7 @@ in
     ./maestral.nix
     ../../modules/timemachine.nix
     ../shared.nix
+    ./sshfs.nix
   ];
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
