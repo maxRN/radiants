dbfdee04b7297310b8ba96edac4e8a7ebd4e382e
Max Große <max12.gro@gmail.com>
2025-08-07 16:00:03 +0200

    only have paperless on tailscale network

--- hosts/kaladin/paperless.nix
+++ hosts/kaladin/paperless.nix
@@ -1,9 +1,9 @@
 { paperless-pkgs, config, ... }:
 let
-  port = 28981;
   config_file = "/var/lib/config-paperless/config";
   password_file = "/var/lib/config-paperless/passwd";
 in
+# make sure to `tailscale serve 28981`
 {
   services.paperless = {
     enable = true;
@@ -16,13 +16,6 @@ in
       PAPERLESS_URL = "https://paperless.maxrn.dev";
       PAPERLESS_OCR_LANGUAGE = "deu+eng";
     };
-  };
-
-  services.caddy = {
-    enable = true;
-    virtualHosts."paperless.maxrn.dev".extraConfig = ''
-      reverse_proxy http://127.0.0.1:${toString port}
-    '';
   };
 
   services.storagebox.members = [ config.services.paperless.user ];
